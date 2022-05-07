1cc4e5b2b58e98b1256426223d52257b69879114
Josh Espinoza <joshespi@gmail.com>
2022-05-07 00:20:45 -0600

    0.0.02
    * Worked on header styling/layout
    * worked on menu including responsive switch
    * added provo school district header items, including logo, menu, search form, address
    * added search icon

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Provo City School District Master Theme
+
+# === Provo City School District Master Theme ===
+License: GPLv2 or later
+License URI: <https://www.gnu.org/licenses/gpl-2.0.html>
+
+## == Description ==
+For Provo City School District Websites
+
+## == Changelog ==
+### = 0.0.02 =
+* Worked on header styling/layout
+* worked on menu including responsive switch
+* added provo school district header items, including logo, menu, search form, address
+* added search icon
+### = 0.0.01 =
+initial structure build with very basic wordpress things
+
+## == Resources ==
+* normalize.css <https://necolas.github.io/normalize.css/>, (C) 2012-2016 Nicolas Gallagher and Jonathan Neal, [MIT](<https://opensource.org/licenses/MIT>)
--- pcsd-master-theme/header.php
+++ pcsd-master-theme/header.php
@@ -9,15 +9,19 @@
     <header id="mainHeader">
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/pcsd-logo-website-header-x2.png" alt="District Logo" />
       <nav>
-        <a href="<?php echo home_url(); ?>">Blog</a>
-        <a href="https://espifam.com/contact-us/">Contact Us</a>
-        <a href="https://espifam.com/privacy-policy/">Privacy Policy</a>
-        <a href="https://github.com/joshespi/espi-theme">Theme</a>
+        <a><i class="fa fa-bars" aria-hidden="true"></i> Menu</a>
+        <ul>
+          <li><a href="<?php echo home_url(); ?>">Blog</a></li>
+          <li><a href="https://espifam.com/contact-us/">Contact Us</a></li>
+          <li><a href="https://espifam.com/privacy-policy/">Privacy Policy</a></li>
+          <li><a href="https://github.com/joshespi/espi-theme">Theme</a></li>
+        </ul>
       </nav>
 
       <form>
+        <label for="search">Search: </label>
         <input type="text" placeholder="Search.." name="search">
-        <button type="submit"><i class="fa fa-search"></i></button>
+        <input type="submit" class="submit" value="search">
       </form>
       <ul class="address" itemtype="https://schema.org/PostalAddress">
         <li itemprop="streetAddress">280 West 940 North Provo, Utah </li>
